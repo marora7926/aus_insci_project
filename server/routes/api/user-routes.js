06e82c1be1ce9021a70c60c2bdd134dcad2209b8
Mohit Arora <mohit.arora@sydney.edu.au>
2021-08-02 19:23:52 +1000

    added dummy routes

---
+++ server/routes/api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+  createUser,
+  getSingleUser,
+  saveBook,
+  deleteBook,
+  login,
+} = require('../../../../book-search-engine/server/controllers/user-controller');
+
+// import middleware
+const { authMiddleware } = require('../../../../book-search-engine/server/utils/auth');
+
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser).put(authMiddleware, saveBook);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
+
+router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+
+module.exports = router;

9bc456fc768004dfe017814f6f89d3e66ef0b11d
Mohit Arora <mohit.arora@sydney.edu.au>
2021-08-03 23:28:45 +1000

    added subpages for involved

--- client/src/pages/GetInvolved.js
+++ client/src/pages/GetInvolved.js
@@ -2,28 +2,41 @@ import React from 'react';
 import { Jumbotron, Container, Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Participate from './involved-subpages/Participate';
+import Ideas from './involved-subpages/Ideas'
+import Contact from './involved-subpages/Contact';
+
 const GetInvolved = () => {
-    console.log('Reach us');
     return (
-        <>
-            <Jumbotron fluid className='text-light bg-dark'>
-                <Container>
-                    <h1>Reach out to us</h1>
-                </Container>
-            </Jumbotron>
-            <Navbar bg='success' variant='dark' expand='lg'>
-                <Container fluid>
-                    <Navbar.Toggle aria-controls='navbar' />
-                    <Navbar.Collapse id='navbar'>
-                        <Nav>
-                            <Nav.Link as={Link} to='/report'>Want to participate?</Nav.Link>
-                            <Nav.Link as={Link} to='/plain'>Share your ideas for change</Nav.Link>
-                            <Nav.Link as={Link} to='/science'>Contact us</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <Router>
+            <>
+                {/* <Jumbotron fluid className='text-light bg-dark'>
+                    <Container>
+                        <h1>Reach out to us</h1>
+                    </Container>
+                </Jumbotron> */}
+                <Navbar bg='info' variant='dark' expand='lg'>
+                    <Container fluid>
+                        <Navbar.Toggle aria-controls='navbar' />
+                        <Navbar.Collapse id='navbar'>
+                            <Nav>
+                                <Nav.Link as={Link} to='/participate'>Want to participate?</Nav.Link>
+                                <Nav.Link as={Link} to='/ideas'>Share your ideas</Nav.Link>
+                                <Nav.Link as={Link} to='/contact'>Contact us</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                <Switch>
+                    <Route exact path='/' component={Participate} />
+                    <Route exact path='/participate' component={Participate} />
+                    <Route exact path='/ideas' component={Ideas} />
+                    <Route exact path='/contact' component={Contact} />
+                </Switch>
+            </>
+        </Router>
     );
 };
 
---
+++ client/src/pages/involved-subpages/participate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Participate = () => {
+    return (
+        <>
+        <h1>Wnat to participate page</h1>
+        </>
+    );
+};
+
+export default Participate;
